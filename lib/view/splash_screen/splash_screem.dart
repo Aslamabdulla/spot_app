eac456e802745a5651a0efbbd86992d91f7c287d
Aslamabdulla <themanwithgunpower@gmail.com>
2022-12-17 22:37:16 +0530

    first_commit

---
+++ lib/view/splash_screen/splash_screem.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class SplashShreen extends StatelessWidget {
+  const SplashShreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
